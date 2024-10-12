c8583430bf93441304785cfa36210c0a9d2e7290
Nived Suresh <niveds406@gmail.com>
2024-10-12 14:33:08 +0530

    init

---
+++ src/main/java/com/jpa/hacks/jpa_hacks/JpaHacksApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.hacks.jpa_hacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHacksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHacksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/hacks/jpa_hacks/JpaHacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.hacks.jpa_hacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHacksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
